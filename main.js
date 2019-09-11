09bdc3a6f34c844a8724c7d77cc5157c736417d9
Paulo Ricardo <paulo0899@gmail.com>
2019-09-11 01:44:51 -0300

    extension

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener('keyup', function (event) {
+    var flag = event.getModifierState && event.getModifierState('CapsLock');
+    var text = document.getElementById("text");
+    var icon = document.getElementById("icon");
+    document.getElementById("icon").innerHTML = (flag ? (
+        chrome.browserAction.setIcon({
+            path : "img/up.png"
+          })
+    ) : (
+        chrome.browserAction.setIcon({
+            path : "img/down.png"
+          })
+        ));
+    document.getElementById("text").innerHTML = (flag ? "ON" : "OFF");
+});
