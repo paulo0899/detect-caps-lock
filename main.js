3e74f26fcf8599c66db4c2bcf9eaba0444556213
Paulo Ricardo <paulo0899@gmail.com>
2020-08-02 18:59:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener('keydown', function (event) {
-    let flag = event.getModifierState && event.getModifierState('CapsLock');
-    let text = document.getElementById("text");
-    let icon = document.getElementById("icon");
+    let flag = event.getModifierState && event.getModifierState('CapsLock')
+    let text = document.getElementById("text")
+    //let icon = document.getElementById("icon")
     document.getElementById("icon").innerHTML = (flag ? (
         chrome.browserAction.setIcon({
             path : "img/up.png"
@@ -10,6 +10,6 @@ document.addEventListener('keydown', function (event) {
         chrome.browserAction.setIcon({
             path : "img/down.png"
           })
-        ));
-    document.getElementById("text").innerHTML = (flag ? "ON" : "OFF");
-});
+        ))
+    document.getElementById("text").innerHTML = (flag ? "ON" : "OFF")
+})
